16d473f950cd98150278c1eb564ef45c9abf2288
girnarhitesh <hiteshgirnar99@gmail.com>
2025-12-07 15:04:06 +0530

    code change

--- backend/Server.js
+++ backend/Server.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
-import roomRoutes from "./routes/roomRoutes.js";
+import roomRoutes from "./routes/room.routes.js";
 
 dotenv.config();
 
@@ -17,15 +17,15 @@ app.use("/api/rooms", roomRoutes);
 
 // Root route
 app.get("/", (req, res) => {
-  res.send("Room Booking Backend is running!");
+    res.send("Room Booking Backend is running!");
 });
 
 // MongoDB connection (latest Mongoose)
 mongoose.connect(process.env.MONGO_URL)
-  .then(() => {
-    console.log("MongoDB connected successfully");
-    app.listen(process.env.PORT, () =>
-      console.log(`Server running on http://localhost:${process.env.PORT}`)
-    );
-  })
-  .catch((err) => console.log(err));
+    .then(() => {
+        console.log("MongoDB connected successfully");
+        app.listen(process.env.PORT, () =>
+            console.log(`Server running on http://localhost:${process.env.PORT}`)
+        );
+    })
+    .catch((err) => console.log(err));
